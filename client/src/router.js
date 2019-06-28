d7edfcb0131ed55e4c838c723246a9f3f7e3fa29
craiggough376 <craig_gough_376@hotmail.com>
2019-06-28 15:19:10 +0100

    client folders and router setup

---
+++ client/src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home';
+import RomeStory '@/views/RomeStory';
+import WW2Story from '@/views/WW2Story';
+import USAStory from '@/views/USAStory';
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/romestory',
+      name: 'rome-story',
+      component: RomeStory
+    },
+    {
+      path: '/ww2story',
+      name: 'ww2-story',
+      component: WW2Story
+    },
+    {
+      path: '/usastory',
+      name: 'usa-story',
+      component: USAStory
+    }
+  ]
+});
+
+export default router;
