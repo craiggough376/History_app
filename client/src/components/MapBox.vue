2060dbb031471a223d4c3d38e457329560b6a19a
craiggough376 <craig_gough_376@hotmail.com>
2019-06-29 14:07:19 +0100

    multiple markers working

--- client/src/components/MapBox.vue
+++ client/src/components/MapBox.vue
@@ -1,23 +1,51 @@
 <template lang="html">
   <div class="map">
-    <p>My Map</p>
+    <p>Map</p>
     <l-map :zoom="zoom" :center="center">
-   <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-   <l-marker :lat-lng="marker"></l-marker>
+   <l-tile-layer :url="url"></l-tile-layer>
+   <l-marker :lat-lng="places[index]['coordinates']">
+     <l-popup>
+       <h1>{{places[index]['title']}}</h1>
+       <p>{{places[index]['blurb']}}</p>
+     </l-popup>
+   </l-marker>
  </l-map>
+ <button v-on:click="handleClick">Next Place</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "map-box",
+  methods: {
+    handleClick(){
+      this.index += 1
+    }
+  },
   data(){
     return{
-      zoom:13,
+      zoom:4,
       center: L.latLng(47.413220, -1.219482),
       url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       marker: L.latLng(47.413220, -1.219482),
+      index: 0,
+      places: [
+        {
+          "title": "London",
+          "blurb": "Blah Blah Blah Blah",
+          "coordinates": L.latLng(51.413220, -1.219482)
+        },
+        {
+          "title": "France",
+          "blurb": "Blah Blah Blah Blah",
+          "coordinates": L.latLng(47.413220, -1.219482)
+        },
+        {
+          "title": "Spain",
+          "blurb": "Blah Blah Blah Blah",
+          "coordinates": L.latLng(40.413220, -1.219482)
+        }
+      ]
     }
   }
 }
@@ -26,7 +54,8 @@ export default {
 <style lang="css" scoped>
 
 .map{
-  height: 50vh;
+  height: 70vh;
+  width: 70vw;
 }
 
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
 import { Icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
@@ -11,6 +11,7 @@ Vue.config.productionTip = false
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
+Vue.component('l-popup', LPopup);
 
 delete Icon.Default.prototype._getIconUrl;
 
