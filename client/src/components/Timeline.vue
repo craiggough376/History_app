3d9e763bcc53a3f91589f95eb4cd31aa1a369c36
Adamsh6 <adam.s.houten@gmail.com>
2019-06-30 16:18:40 +0100

    can change selected episode by clicking it

--- client/src/components/Timeline.vue
+++ client/src/components/Timeline.vue
@@ -1,19 +1,20 @@
 <template lang="html">
 <div>
-  <div class="grid">
+  <div class="grid" :style="gridStyle">
     <div class="start-date">1980</div>
     <div class="end-date">1995</div>
     <div class="timeline"></div>
-    <div class="episode0branch"></div>
-    <div class="episode1branch"></div>
-    <div v-for="(episode, index) in episodes" :class="'episode'+index" :style="styles[index]">
+    <div v-for="(episode, index) in episodes" :class="'episode'+index" :style="styles[index]" v-on:click="handleClick(index)">
       <p>{{episode.date}}</p>
       <p>{{episode.title}}</p>
     </div>
     <div v-for="(episode, index) in episodes" :class="'episode'+index+'branch'" :style="branches[index]" style="">
     </div>
-    <div class="episodexbranch"></div>
+    <div class="episodedown"></div>
+    <div class="episodedownbranch"></div>
   </div>
+  <button v-on:click="handlePrevious">Previous</button>
+  <button v-on:click="handleNext">Next</button>
 </div>
 </template>
 
@@ -30,16 +31,39 @@ export default {
       {date: 1986, title: "This happened in another time"},
       {date: 1986, title: "This happened in another time"},
       {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
+      {date: 1986, title: "This happened in another time"},
     ],
-    boxWidth: 4,
+    boxWidth: 8,
     boxGap: 1,
-    selectedEpisode: 1
+    selectedEpisode: 0,
+    highlightColor: 'powderblue',
+    gridsize: 100
     }
   },
   computed: {
+    gridStyle(){
+      return {
+        'grid-template-columns': 'repeat(' + this.gridsize + ', 1fr)'
+      }
+    },
     styles() {
       const noOfEpisodes = this.episodes.length
-      const episodeGap = Math.floor(43/(noOfEpisodes))
+      console.log(noOfEpisodes)
+      let episodeGap = Math.floor((this.gridsize + noOfEpisodes)/(noOfEpisodes))
+      if(episodeGap <2){
+        episodeGap = 2;
+      }
+      console.log(episodeGap)
       const stylesArray = []
       let startingColumn = 2
       let endingColumn = startingColumn + this.boxWidth
@@ -47,35 +71,56 @@ export default {
         const baseStyle = {
           border: '1px solid black',
           'grid-column': '',
-          'grid-row': '3/5',
+          'grid-row': '1/3',
           overflow: 'scroll',
         }
         if(i == this.selectedEpisode){
-          baseStyle['background-color'] = 'blue'
+          baseStyle['background-color'] = this.highlightColor
         }
         stylesArray.push(baseStyle);
+        if(i % 2 == 1){
+          stylesArray[i]['grid-row'] = '5/7'
+        }
         stylesArray[i]['grid-column'] = startingColumn + '/' + endingColumn;
-        startingColumn += (1 + episodeGap);
-        endingColumn += (1 + episodeGap);
+        startingColumn += (episodeGap -1);
+        endingColumn += (episodeGap -1);
       }
       return stylesArray
     },
     branches() {
       const noOfEpisodes = this.episodes.length
-      const episodeGap = Math.floor(43/(noOfEpisodes))
+      const episodeGap = Math.floor((this.gridsize + noOfEpisodes)/(noOfEpisodes))
       const stylesArray = []
       let column = 1 + this.boxWidth/2
       for(let i=0; i < this.episodes.length; i++){
         const baseStyle = {
           'border-right': '1px solid black',
           'grid-column': '',
-          'grid-row': '5/6',
+          'grid-row': '3/4',
         }
         stylesArray.push(baseStyle);
         stylesArray[i]['grid-column'] = column + '/' + column;
-        column += (1 + episodeGap);
+        if(i % 2 == 1){
+          stylesArray[i]['grid-row'] = '4/5'
+        }
+        column += (episodeGap -1);
       }
       return stylesArray
+    }
+  },
+  methods: {
+    handleNext() {
+      if(this.selectedEpisode < this.episodes.length -1){
+        this.selectedEpisode += 1
+      }
+    },
+    handlePrevious() {
+      if(this.selectedEpisode > 0){
+        this.selectedEpisode -= 1
+      }
+    },
+    handleClick(index) {
+      this.selectedEpisode = index
     }
   }
 
@@ -85,28 +130,44 @@ export default {
 <style lang="css" scoped>
 .grid{
   display: grid;
-  grid-template-columns: repeat(50, 1fr);
-  grid-template-rows: repeat(10, 1fr);
-  height: 500px;
-  width: 100%;
+  /* grid-template-columns: repeat(50, 1fr); */
+  grid-template-rows: repeat(6, 1fr);
+  height: 280px;
+  width: 90%;
   border: 1px solid blue;
+  padding: 10px;
+  border-radius: 1em;
 }
 .box {
   border: 2px solid black;
 }
 .start-date {
   grid-column: 1 / 3;
-  grid-row: 6 / 7;
+  grid-row: 4 / 5;
 }
 .end-date {
-  grid-column: 49 / 51;
-  grid-row: 6 / 7;
+  grid-column: 97 / 101;
+  grid-row: 4 / 5;
 }
 .timeline {
   border-bottom: 1px solid black;
-  grid-column: 3 / 49;
-  grid-row: 5 / 6;
+  grid-column: 3 / 98;
+  grid-row: 3 / 4;
 }
+/* .episodedown {
+  border: 1px solid black;
+  grid-column: 4/8;
+  grid-row: 7/9;
+  overflow: scroll;
+} */
+
+/* .episodedownbranch {
+  border-right: 1px solid black;
+  grid-column: 5/5;
+  grid-row: 6/7
+
+} */
+
 /* .episode0{
   border: 1px solid black;
   grid-column: 2/10;
