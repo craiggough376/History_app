a626b06a70b39b06474da7498b59b116e5838f4f
Adamsh6 <adam.s.houten@gmail.com>
2019-07-01 14:10:50 +0100

    added styling options for timeline

--- client/src/views/WW2Story.vue
+++ client/src/views/WW2Story.vue
@@ -2,7 +2,7 @@
   <div>
   <h1>World War 2</h1>
   <map-box :selectedEpisode="selectedEpisode" :events="filteredEvents"/>
-  <timeline :selectedEpisode="selectedEpisode" :events="filteredEvents"/>
+  <timeline :selectedEpisode="selectedEpisode" :events="filteredEvents" :inheritedStyle="inheritedStyle" :boxWidth="boxWidth"/>
 </div>
 </template>
 
@@ -23,6 +23,20 @@ export default {
   components: {
     "timeline": Timeline,
     "map-box": MapBox
+  },
+  data() {
+    return {
+      boxWidth: 8,
+      inheritedStyle: {
+        box: {},
+        branch: {},
+        grid: {},
+        timeline: {},
+        boundaries: {},
+        date: {},
+        title: {}
+      }
+    }
   }
 }
 </script>
