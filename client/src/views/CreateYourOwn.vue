04f058a823d7776e95bac7cf7272718861d57ff8
Adamsh6 <adam.s.houten@gmail.com>
2019-07-02 11:38:37 +0100

    working map with timeline

--- client/src/views/CreateYourOwn.vue
+++ client/src/views/CreateYourOwn.vue
@@ -1,24 +1,33 @@
 <template lang="html">
-  <div>
-<form class="" v-on:submit.prevent="handleSubmit" type="post">
+  <div class="all">
+<form v-if="formOn" class="" v-on:submit.prevent="handleSubmit" type="post">
   <label for="title">Event:</label>
   <input id="title" type="text" name="" v-model="title" required>
   <label for="date">Date:</label>
   <input id="date" type="text" name="" v-model="date" required>
+  <input id="coordinates" type="text" v-model="coordinates" required>
   <input type="submit" name="" value="Add Event">
 </form>
+<button v-if="formOn" v-on:click="handleFinish" type="button" name="button">Finished</button>
+<event v-if="!(formOn)" :event="episodes[selectedEpisode]"/>
 <timeline selectedEpisode="selectedEpisode" :events="episodes" :inheritedStyle="inheritedStyle" :boxWidth="boxWidth" :dateBreadth="dateBreadth"/>
-<map-box :selectedEpisode="selectedEpisode" :events="filteredEvents" :mapUrl="mapUrl" :center="center"/>
+<map-box :selectedEpisode="selectedEpisode" :events="episodes" :mapUrl="mapUrl" :center="center"/>
 </div>
 </template>
 
 <script>
 import Timeline from '@/components/Timeline'
+import MapBox from '@/components/MapBox'
+import Event from '@/components/Event'
+import {eventBus} from '../main.js'
+
 export default {
   data() {
     return {
+      formOn: true,
       title: "",
       date: "",
+      coordinates: "",
       episodes: [],
       boxWidth: 8,
       inheritedStyle: {
@@ -29,13 +38,22 @@ export default {
         boundaries: {},
         date: {},
         title: {},
-        selected: {}
+        selected: {
+          "font-weight": "bold",
+        }
       },
       dateBreadth: {
         start: '',
         end: ''
-      }
+      },
+      mapUrl: 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png',
+      center: [50.51342652633956, 13.0078125]
     }
+  },
+  mounted() {
+    eventBus.$on('map clicked', (payload) => {
+      this.coordinates = payload;
+    })
   },
   methods: {
     handleSubmit() {
@@ -43,18 +61,26 @@ export default {
       this.episodes.push(
         {
           title: this.title,
-          date: this.date
+          date: this.date,
+          coordinates: this.coordinates
         }
       )
       this.dateBreadth.end = this.date
       if(this.episodes.length == 1){
         this.dateBreadth.start = this.date
       }
+    },
+    handleFinish() {
+      console.log('finished')
+      this.formOn = false;
     }
   },
   components: {
-    "timeline": Timeline
+    "timeline": Timeline,
+    "event": Event,
+    "map-box": MapBox
   },
+  props: ['selectedEpisode']
 }
 </script>
 
