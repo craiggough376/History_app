b8854f93a1886122436b1a7b6ff79397ef6c9a49
Adamsh6 <adam.s.houten@gmail.com>
2019-07-02 10:04:24 +0100

    created view and base form

---
+++ client/src/views/CreateYourOwn.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div>
+<form class="" v-on:submit.prevent="handleSubmit">
+  <label for="title">Event:</label>
+  <input id="title" type="text" name="" v-model="title" required>
+  <label for="date">Date:</label>
+  <input id="date" type="text" name="" v-model="date" required>
+  <input type="submit" name="" value="Add Event">
+</form>
+<div class="events-list">
+  <div v-for="episode in episodes">
+    {{episode.title}}
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      date: "",
+      episodes: []
+    }
+  },
+  methods: {
+    handleSubmit() {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
